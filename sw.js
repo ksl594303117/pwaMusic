be18d1d222733d785e9d6cd7a930af656855340b
 寇姝玲 <koushuling@soyoung.com>
2019-04-25 16:28:19 +0800

    通信，备注

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 importScripts( 'https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js' )
-var cacheStorageKey = 'minimal-pwa-2'
+var cacheStorageKey = 'minimal-pwa-0'
 // 定义需要缓存的路径以及需要缓存的静态文件的列表
 var bigCacheList = [
   '/',
   'index.html',
-  'sw.js',
   'src/assets/css/main.css'
 ]
 var smallCacheList = [
@@ -34,21 +33,40 @@ self.addEventListener( 'install', e => {
         // 稳定文件或者小文件加载
         return cache.addAll( smallCacheList )
       } )
-      .then( () => self.skipWaiting() ) // self.skipWaiting()是为了在页面更新的过程中，新的sw脚本能够立刻激活和生效
+      // 正常流程是：旧的sw不工作的时候，新的sw才会起作用，但是浏览器何时关闭sw并不确定，因此使用：
+      // self.skipWaiting()-是为了在页面更新的过程中，新的sw脚本能够立刻激活和生效
+      .then( () => self.skipWaiting() )
   )
 } )
 
 // 缓存捕获-处理动态缓存，需要监听fetch事件，在caches中取match事件的request
 self.addEventListener( 'fetch', e => {
   console.log( 'fetch', e )
+  // respondWith 用来包含响应主页面请求的代码。
   e.respondWith(
-    // caches：用来控制缓存专门分离出来的一个对象
+    // caches：用来控制缓存专门分离出来的一个对象。是根据event.request在缓存空间中查找指定路径的缓存文件，
+    // 如果匹配到，那么response是有内容的，如果没有，通过fetch进行捕获，此时可以手动fetch然后添加进缓存。
     caches.match( e.request ).then( response => {
       if ( response ) {
         return response
       }
       // fetch：现代浏览器用来替代XMLHttpRequest专门开发出的ajax请求。
-      return fetch( e.request )
+      // 因为event.request 流已经在caches.match中使用过一次，所以不能再次使用，故得到副本再使用。
+      var fetchRequest = event.request.clone()
+      return fetch( fetchRequest ).then( res => {
+        // 失败-直接返回res
+        if ( !res || res.status !== 200 || res.type !== 'basic' ) {
+          return res
+        }
+        // 成功，继续：该res一要给浏览器渲染，二要进行缓存。
+        // 由于caches.put使用的是文件的响应流，一旦使用，返回的res就无法访问造成失败，所以需要复制一份。
+        var resToCache = res.clone()
+        caches.open( cacheStorageKey )
+          .then( cache => {
+            cache.put( event.request, resToCache )
+          } )
+        return res
+      } )
     } ).catch( err => {
       console.error( 'Fetching failed:', err )
       throw err
@@ -60,11 +78,13 @@ self.addEventListener( 'activate', e => {
   e.waitUntil(
     // 获取所有cache名称
     caches.keys().then( cachesNames => {
+      console.log( 'cachesNames', cachesNames )
       return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
+        // 获取所有不同于当前版本名称cache下的内容,删除其他版本缓存的文件
         cachesNames.filter( cachesName => {
           return cachesName !== cacheStorageKey
         } ).map( cachesName => {
+          console.log( 'delete cachesName', cachesName )
           return caches.delete( cachesName )
         } )
       )
@@ -74,4 +94,8 @@ self.addEventListener( 'activate', e => {
       return self.clients.claim()
     } )
   )
+} )
+
+self.addEventListener( 'message', e => {
+  console.log( 'receive message', e, ',data:', e.data )
 } )
